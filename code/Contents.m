d501b905b222f1d8b247b117d58a7957c6743720
davidnbresch <david.bresch@gmail.com>
2014-04-24 21:38:38 +0200

    tiny edit

--- code/Contents.m
+++ code/Contents.m
@@ -1,3 +1,3 @@
 % climada - economics of climate adaptation - demo for Barisal, Bangladesh
 % get started with climada_demo, see ECA_Barisal_handson.pdf in ../docs
-% david.bresch@gmail.com
+% david.bresch@gmail.com 
