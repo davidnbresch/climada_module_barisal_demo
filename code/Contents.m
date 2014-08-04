3fbd51f8ce1530eeb16a7c1fa6bce1739c906a6c
davidnbresch <david.bresch@gmail.com>
2014-08-04 21:28:23 +0200

    TC, TR and TC hazard generation
    see bangladesh_hazard_TEST to test generation of TC, TR and TS hazard
    event sets for Bangladesh

--- code/Contents.m
+++ code/Contents.m
@@ -1,3 +1,3 @@
 % climada - economics of climate adaptation - demo for Barisal, Bangladesh
-% get started with climada_demo, see ECA_Barisal_handson.pdf in ../docs
+% get started with climada_demo, then bangladesh_hazard_demo, see ECA_Barisal_handson.pdf in ../docs
 % david.bresch@gmail.com
