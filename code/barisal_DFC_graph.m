066c1176f68a70d867c9100b42df5370f9b179ea
muellele <muellele@gmail.com>
2015-06-23 17:12:23 +0200

    updated DFC graphs (frequency bug in cyclone flooding in cc scenarios)

--- code/barisal_DFC_graph.m
+++ code/barisal_DFC_graph.m
@@ -28,6 +28,14 @@ for i = 1:EDS_no
     [~,~,return_period,sorted_damage] = climada_EDS_DFC(EDS(i));
     close all
     no = numel(return_period);
+    if strcmp(EDS(i).annotation_name,'Barisal_BCC_hazard_FL_depth_cyclone_cc_2030_moderate') |...
+            strcmp(EDS(i).annotation_name,'Barisal_BCC_hazard_FL_duration_cyclone_cc_2030_moderate');
+        return_period = return_period*1.009;
+    end
+    if strcmp(EDS(i).annotation_name,'Barisal_BCC_hazard_FL_depth_cyclone_cc_2050_moderate') |...
+            strcmp(EDS(i).annotation_name,'Barisal_BCC_hazard_FL_duration_cyclone_cc_2050_moderate');
+        return_period = return_period*1.018;
+    end
     matr(3:3+no-1,(i-1)*2+1) = num2cell(return_period);
     matr(3:3+no-1,(i-1)*2+2) = num2cell(sorted_damage);
 end
@@ -36,4 +44,15 @@ xlswrite(xls_file,matr)
 
 
 %%
+climada_figuresize(0.5,0.9);
+[fig,legend_str,return_period,sorted_damage] = climada_EDS_DFC(EDS([3 4 1 2 5]));
+climada_figuresize(0.5,0.9);
+[fig,legend_str,return_period,sorted_damage] = climada_EDS_DFC(EDS([8 9 6 7 10]));
+
+% comparison of moonsoon and cyclone flooding (depth damage only, as this
+% is the driver of losses) in 2014 and 2030 (scenario 1, moderate climate change)
+climada_figuresize(0.5,0.9);
+[fig,legend_str,return_period,sorted_damage] = climada_EDS_DFC(EDS([3 8 1 6]));
+print(fig,'-dpdf',[results_dir filesep 'DFC_2014_2030_monsoon_vs_cyclone_flooding.pdf'])
+
 
